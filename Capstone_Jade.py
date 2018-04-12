60dfac3c9ff627d95afec46037e005a3492110be
18jae42 <37602262+18jae42@users.noreply.github.com>
2018-04-12 14:00:48 +0900

    Add files via upload

--- Capstone_Jade.py
+++ Capstone_Jade.py
@@ -15,6 +15,7 @@ class Player(spgl.Sprite):
 	def __init__(self, shape, color, x, y,speed):
 		spgl.Sprite.__init__(self, shape, color, x, y)
 		self.speed = 1
+		self.ticks = 0
 		
 	def go_forward(self):
 		self.fd(10)
@@ -28,14 +29,37 @@ class Player(spgl.Sprite):
 	def turn_right(self):
 		self.rt(30)
 		
+	def tick(self):
+		self.ticks += 1
+		if self.ticks == 30:
+			game.timer -= 1
+			lbl_time.update("Time Left: {} seconds".format(game.timer))
+			self.ticks = 0
+			
+class Border(turtle.Turtle):
+	def __init__(self):
+		turtle.Turtle.__init__(self)
+		self.penup() 
+		self.hideturtle()
+		self.speed(0)
+		self.color("black")
+		self.pensize(5)
 	
-	
+	def draw_border(self):
+		self.penup()
+		self.goto(-350,-350)
+		self.pendown()
+		self.goto(-350,350)
+		self.goto(350,350)
+		self.goto(350,-350)
+		self.goto(-350,-350)
+		
 class Black(spgl.Sprite):
 	def __init__(self, shape, color, x, y):
 		spgl.Sprite.__init__(self, shape, color, x, y)
 
 	def place_random(self):
-		self.goto(random.randint(-400, 400), random.randint(-400,400))
+		self.goto(random.randint(-350, 350), random.randint(-350,350))
 		
 
 class Color(spgl.Sprite):
@@ -43,7 +67,7 @@ class Color(spgl.Sprite):
 		spgl.Sprite.__init__(self, shape, color, x, y) 
         
 	def place_random(self):
-		self.goto(random.randint(-400, 400), random.randint(-400,400))    
+		self.goto(random.randint(-350, 350), random.randint(-350,350))    
     
         
 class Boxes(spgl.Sprite):
@@ -57,29 +81,26 @@ class Boxes(spgl.Sprite):
 # Initial Game setup
 game = spgl.Game(800, 800, "grey", "Collect the Rainbow",0)
 game.lives = 3 
-game.time = 120 
+game.timer = 120 
 
 # Create Sprites
 player = Player("triangle", "yellow", 200, -200, 1)
+border = Border()
 box = Boxes("square", "white", 100, 0)
-black = Black("circle", "black", 0, 0)
-color1 = Color("circle", "red", 0, 0)
-color2 = Color("circle", "orange", 0, 0)
-color3 = Color("circle", "yellow", 0, 0)
-color4 = Color("circle", "green", 0, 0)
-color5 = Color("circle", "blue", 0, 0)
-color6 = Color("circle", "navy", 0, 0)
-color7 = Color("circle", "purple", 0, 0)
+black = Black("circle", "black", 1500, 1500)
+color1 = Color("circle", "red", 1500, 1500)
+color2 = Color("circle", "orange", 1500, 1500)
+color3 = Color("circle", "yellow", 1500, 1500)
+color4 = Color("circle", "green", 1500, 1500)
+color5 = Color("circle", "blue", 1500, 1500)
+color6 = Color("circle", "navy", 1500, 1500)
+color7 = Color("circle", "purple", 1500, 1500)
+
+
+border.draw_border()
 
 # Create Functions
-def isCollision(object1,object2):
-	a = object1.xcor() - object2.xcor()
-	b = object1.ycor()- object2.ycor()
-	d = math.sqrt((a**2)+(b**2))
-	if d < 10:
-		return True 
-	else:
-		return False
+
 
 def place_circle():
 	computer_choice = random.randint(1,2)
@@ -102,8 +123,17 @@ def place_circle():
 		elif color_choice == 7:
 			color7.place_random()
 
-
 # Create Labels
+lbl_lives = spgl.Label("Lives left: 3", "red", -350, 360)
+
+lbl_lives.set_font_name("Georgia") 
+lbl_lives.set_font_size(20) 
+
+lbl_time = spgl.Label("Time Left", "Blue", 170, 360)
+lbl_time.update("Time Left: {} seconds".format(game.timer))
+
+lbl_time.set_font_name("Georgia") 
+lbl_time.set_font_size(20) 
 
 # Create Buttons
 
@@ -114,17 +144,34 @@ turtle.onkey(player.go_forward, "Up")
 turtle.onkey(player.go_backward, "Down")
 turtle.onkey(player.turn_right, "Right")
 
+rainbow = 0 
 
 while True:
     # Call the game tick method
-    game.tick()
-    if game.is_collision(player, box):
-    	box.destroy()
-    	place_circle()
-    	print("COLLISION")
-    
-    #if game.is_collision(player, color):
-    	#if color
+	game.tick()
+	#start the game time
+	if game.is_collision(player, box):
+		box.destroy()
+		place_circle()
+		print("COLLISION")
+
+	if game.is_collision(player, black):
+		black.destroy()
+		game.lives -= 1	
+		lbl_lives.update("Lifes Left: {}".format(game.lives))
+    	
+	if game.is_collision(player, color1):
+		rainbow += 1
+		color1.destroy()
+	
+	if rainbow == 7:
+		print("You win!")
+	
+	if game.lives <= 0:
+		quit()
+		
+	if game.timer == 0:
+		quit()
     	
     
     	
